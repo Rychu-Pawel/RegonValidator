a738f125d1171abc82336209452eb7c19c58d670
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-03-06 21:55:15 +0100

    Implemented regon validator and tests

---
+++ src/RegonValidator/RegonValidator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace Rychusoft.Validators
+{
+    public static class RegonValidator
+    {
+        private static readonly int[] shortRegonWeights = { 8, 9, 2, 3, 4, 5, 6, 7 };
+        private static readonly int[] longRegonWeights = { 2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8 };
+
+        public static bool IsValid(string regon)
+        {
+            if (IsEmptyOrNotProperLength(regon))
+                return false;
+
+            if (!IsNumber(regon))
+                return false;
+
+            if (!IsChecksumValid(regon))
+                return false;
+
+            if (!HasAtLeastThreeUniqueDigits(regon))
+                return false;
+
+            return true;
+        }
+
+        private static bool IsEmptyOrNotProperLength(string regon)
+        {
+            return string.IsNullOrWhiteSpace(regon) || (regon.Length != 9 && regon.Length != 14);
+        }
+
+        private static bool IsNumber(string regon)
+        {
+            return regon.All(Char.IsDigit);
+        }
+
+        private static bool IsChecksumValid(string regon)
+        {
+            char inputRegonChecksum = regon.Length == 9 ? regon[8] : regon[13];
+            int[] weights = regon.Length == 9 ? shortRegonWeights : longRegonWeights;
+
+            int computedChecksum = CalculateChecksum(regon, weights);
+
+            return inputRegonChecksum.ToString() == computedChecksum.ToString();
+        }
+
+        private static int CalculateChecksum(string regon, int[] weights)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < weights.Length; i++)
+                sum += weights[i] * int.Parse(regon[i].ToString());
+
+            int checksum = sum % 11;
+
+            if (checksum == 10)
+                checksum = 0;
+
+            return checksum;
+        }
+
+        private static bool HasAtLeastThreeUniqueDigits(string regon)
+        {
+            return regon.Distinct().Count() > 2;
+        }
+    }
+}
---
+++ tests/RegonValidatorTests/RegonValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using Rychusoft.Validators;
+using System;
+using Xunit;
+
+namespace RegonValidatorTests
+{
+    public class RegonValidatorTests
+    {
+        [Theory]
+        [InlineData("280196813")]
+        [InlineData("443138072")]
+        [InlineData("371358961")]
+        [InlineData("893677596")]
+        [InlineData("550519667")]
+        [InlineData("408888521")]
+        [InlineData("575064641")]
+        [InlineData("300175026")]
+        [InlineData("118315956")]
+        [InlineData("673459600")]
+        [InlineData("54350420840255")]
+        [InlineData("92630057385954")]
+        [InlineData("10326901843754")]
+        [InlineData("80620880514318")]
+        [InlineData("58944905793760")]
+        [InlineData("38882530059365")]
+        [InlineData("81696011691646")]
+        [InlineData("06009873665180")]
+        [InlineData("14974233540206")]
+        [InlineData("14215866256643")]
+        public void IsValid_ValidRegonsTest(string regon)
+        {
+            Assert.True(RegonValidator.IsValid(regon));
+        }
+
+        [Theory]
+        [InlineData("39437971")]
+        [InlineData("8784066")]
+        [InlineData("709471")]
+        [InlineData("86740")]
+        [InlineData("7140")]
+        [InlineData("514")]
+        [InlineData("84")]
+        [InlineData("6")]
+        public void IsValid_TooShortRegonsTest(string regon)
+        {
+            Assert.False(RegonValidator.IsValid(regon));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void IsValid_EmptyRegonsTest(string regon)
+        {
+            Assert.False(RegonValidator.IsValid(regon));
+        }
+
+        [Theory]
+        [InlineData("6275273253")]
+        [InlineData("62752732531")]
+        [InlineData("394379712012")]
+        [InlineData("8784066227133")]
+        [InlineData("709471124714564")]
+        [InlineData("7094711247145648")]
+        [InlineData("70947112471456472")]
+        [InlineData("709471124714564722")]
+        public void IsValid_TooLongRegonsTest(string regon)
+        {
+            Assert.False(RegonValidator.IsValid(regon));
+        }
+
+        [Theory]
+        [InlineData("         ")]
+        [InlineData("        0")]
+        [InlineData("0        ")]
+        [InlineData("      0  ")]
+        [InlineData(";ierhf;dr")]
+        [InlineData("000000000")]
+        [InlineData("111111111")]
+        [InlineData("101010101")]
+        [InlineData("010101010")]
+        [InlineData("aaaaaaaaa")]
+        [InlineData(";lizseduj")]
+        [InlineData("999999999")]
+        [InlineData("              ")]
+        [InlineData("             0")]
+        [InlineData("0             ")]
+        [InlineData("      0       ")]
+        [InlineData(";ierhf;drfgwdv")]
+        [InlineData("00000000000000")]
+        [InlineData("11111111111111")]
+        [InlineData("10101010101010")]
+        [InlineData("01010101010101")]
+        [InlineData("aaaaaaaaaaaaaa")]
+        [InlineData(";lizsedujfgevn")]
+        [InlineData("99999999999999")]
+        public void IsValid_MonkeyInvalidRegonsTest(string regon)
+        {
+            Assert.False(RegonValidator.IsValid(regon));
+        }
+    }
+}
