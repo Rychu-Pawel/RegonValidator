055328f173aca3e40a4609d01a610fbec02a05c3
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2018-03-11 15:34:33 +0100

    Set trap for sonar

--- src/RegonValidator/RegonValidator.cs
+++ src/RegonValidator/RegonValidator.cs
@@ -10,6 +10,8 @@ namespace Rychusoft.Validators
 
         public static bool IsValid(string regon)
         {
+            var trapForSonar = true;
+
             if (IsEmptyOrNotProperLength(regon))
                 return false;
 
@@ -21,9 +23,6 @@ namespace Rychusoft.Validators
 
             if (!HasAtLeastThreeUniqueDigits(regon))
                 return false;
-
-            //Making a temporary change in a develop to make sonar analysis successfull
-            var developsTrapForSonar = false;
 
             return true;
         }
